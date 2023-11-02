2ead09ed04cab0f2b3b9db8dec90fe12c955d6ab
demianov-an <Alexm92@inbox.ru>
2023-11-02 18:42:44 +0600

    add Slider (React-alice-carousel)

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,42 +1,37 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import '../../styles/slider.css'
+import {S} from './Slyder_Styles';
+
+type SlidePropsType = {
+  text: string
+  userName: string
+}
+
+const Slide = (props: SlidePropsType) => {
+  return (
+      <S.Slide>
+        <S.Text>{props.text}</S.Text>
+        <S.Name>@{props.userName}</S.Name>
+      </S.Slide>
+  )
+}
 
 const items = [
-  <div className="item" data-value="1">1</div>,
-  <div className="item" data-value="2">2</div>,
-  <div className="item" data-value="3">3</div>,
-  <div className="item" data-value="4">4</div>,
-  <div className="item" data-value="5">5</div>,
+  <Slide userName={"ivan ivanow"}
+         text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>,
+  <Slide userName={"piotr petrov"}
+         text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>,
+  <Slide userName={"anton antonov"}
+         text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}/>,
 ];
 
 export const Slider = () => (
-    <AliceCarousel
-        mouseTracking
-        items={items}
-    />
+    <S.Slider>
+      <AliceCarousel
+          mouseTracking
+          items={items}
+      />
+    </S.Slider>
 );
-
-
-// import React from 'react';
-// import {FlexWrapper} from "../FlexWrapper";
-// import { S } from './Slyder_Styles';
-//
-// export const Slider: React.FC = () => {
-//   return (
-//       <S.Slider>
-//         <FlexWrapper>
-//           <S.Slide>
-//             <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
-//               et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</S.Text>
-//             <S.Name>@ivan ivanow</S.Name>
-//           </S.Slide>
-//         </FlexWrapper>
-//         <S.Pagination>
-//           <span></span>
-//           <span className="active"></span>
-//           <span></span>
-//         </S.Pagination>
-//       </S.Slider>
-//   );
-// };
